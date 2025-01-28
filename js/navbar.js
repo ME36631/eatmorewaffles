f7b1aadf6d96f5dbc19c98cd652e2dda1c40fc51
Rees Olsen <rees.olsen@outlook.com>
2025-01-27 17:41:32 -0700

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -5,7 +5,7 @@ const avatarUrl = userAvatar ? userAvatar : defaultAvatar;
 const navbarHTML = `<div class="navbar">
 <div class="left">
 <a href="/home" class="logo">
-<img src="/eatmorewaffles/images/favicon.png" alt="Waffles Logo" class="waffleimg" height="50" width="50">
+<img src="https://me36631.github.io/eatmorewaffles/images/favicon.png" alt="Waffles Logo" class="waffleimg" height="50" width="50">
 </a>
 <h3 class="navbar-title">Waffles</h3>
 </div>
@@ -16,13 +16,13 @@ const navbarHTML = `<div class="navbar">
 </a>
 <div class="separator"></div> <!-- Line separator between profile and buttons -->
 <nav class="navbar-links">
-<a href="/eatmorewaffles/home"><i class="fa fa-home fa-lg"></i></a>
-<a href="/eatmorewaffles/games"><i class="fa fa-gamepad fa-lg"></i></a>
-<a href="/eatmorewaffles/music"><i class="fa fa-music fa-lg"></i></a>
-<a href="/eatmorewaffles/theater"><i class="fa fa-tv fa-lg"></i></a>
-<a href="/eatmorewaffles/forms"><i class="fa fa-clipboard-list fa-lg"></i></a>
-<a href="/eatmorewaffles/reviews"><i class="fa fa-star fa-lg"></i></a>
-<a href="/eatmorewaffles/contact" class="margin"><i class="fa fa-user fa-lg"></i></a>
+<a href="https://me36631.github.io/eatmorewaffles/home"><i class="fa fa-home fa-lg"></i></a>
+<a href="https://me36631.github.io/eatmorewaffles/games"><i class="fa fa-gamepad fa-lg"></i></a>
+<a href="https://me36631.github.io/eatmorewaffles/music"><i class="fa fa-music fa-lg"></i></a>
+<a href="https://me36631.github.io/eatmorewaffles/theater"><i class="fa fa-tv fa-lg"></i></a>
+<a href="https://me36631.github.io/eatmorewaffles/forms"><i class="fa fa-clipboard-list fa-lg"></i></a>
+<a href="https://me36631.github.io/eatmorewaffles/reviews"><i class="fa fa-star fa-lg"></i></a>
+<a href="https://me36631.github.io/eatmorewaffles/contact" class="margin"><i class="fa fa-user fa-lg"></i></a>
 </nav>
 </div>
 </div>
