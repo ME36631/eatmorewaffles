7afb4f6c417e277ac390a2935382aeb426361cc4
Rees Olsen <rees.olsen@outlook.com>
2025-01-27 17:44:23 -0700

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,31 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
-const defaultAvatar = "/eatmorewaffles/images/user.png";
-let userAvatar = null;
-const avatarUrl = userAvatar ? userAvatar : defaultAvatar;
-const navbarHTML = `<div class="navbar">
-<div class="left">
-<a href="/home" class="logo">
-<img src="https://me36631.github.io/eatmorewaffles/images/favicon.png" alt="Waffles Logo" class="waffleimg" height="50" width="50">
-</a>
-<h3 class="navbar-title">Waffles</h3>
-</div>
-<div class="right">
-<a href="/eatmorewaffles/profile" class="user-profile">
-<img id="user-avatar" src="${avatarUrl}" alt="" class="avatar">
-<span id="user-name">Sign Up</span>
-</a>
-<div class="separator"></div> <!-- Line separator between profile and buttons -->
-<nav class="navbar-links">
-<a href="https://me36631.github.io/eatmorewaffles/home"><i class="fa fa-home fa-lg"></i></a>
-<a href="https://me36631.github.io/eatmorewaffles/games"><i class="fa fa-gamepad fa-lg"></i></a>
-<a href="https://me36631.github.io/eatmorewaffles/music"><i class="fa fa-music fa-lg"></i></a>
-<a href="https://me36631.github.io/eatmorewaffles/theater"><i class="fa fa-tv fa-lg"></i></a>
-<a href="https://me36631.github.io/eatmorewaffles/forms"><i class="fa fa-clipboard-list fa-lg"></i></a>
-<a href="https://me36631.github.io/eatmorewaffles/reviews"><i class="fa fa-star fa-lg"></i></a>
-<a href="https://me36631.github.io/eatmorewaffles/contact" class="margin"><i class="fa fa-user fa-lg"></i></a>
-</nav>
-</div>
-</div>
-`;
-document.getElementById('navbar-container').innerHTML = navbarHTML;
+  const defaultAvatar = "/eatmorewaffles/images/user.png";
+  let userAvatar = null;
+  const avatarUrl = userAvatar ? userAvatar : defaultAvatar;
+  const navbarHTML = `<div class="navbar">
+    <div class="left">
+      <a href="/home" class="logo">
+        <img src="https://me36631.github.io/eatmorewaffles/images/favicon.png" alt="Waffles Logo" class="waffleimg" height="50" width="50">
+      </a>
+      <h3 class="navbar-title">Waffles</h3>
+    </div>
+    <div class="right">
+      <a href="/eatmorewaffles/profile" class="user-profile">
+        <img id="user-avatar" src="${avatarUrl}" alt="" class="avatar">
+        <span id="user-name">Sign Up</span>
+      </a>
+      <div class="separator"></div> <!-- Line separator between profile and buttons -->
+      <nav class="navbar-links">
+        <a href="https://me36631.github.io/eatmorewaffles/home"><i class="fa fa-home fa-lg"></i></a>
+        <a href="https://me36631.github.io/eatmorewaffles/games"><i class="fa fa-gamepad fa-lg"></i></a>
+        <a href="https://me36631.github.io/eatmorewaffles/music"><i class="fa fa-music fa-lg"></i></a>
+        <a href="https://me36631.github.io/eatmorewaffles/theater"><i class="fa fa-tv fa-lg"></i></a>
+        <a href="https://me36631.github.io/eatmorewaffles/forms"><i class="fa fa-clipboard-list fa-lg"></i></a>
+        <a href="https://me36631.github.io/eatmorewaffles/reviews"><i class="fa fa-star fa-lg"></i></a>
+        <a href="https://me36631.github.io/eatmorewaffles/contact" class="margin"><i class="fa fa-user fa-lg"></i></a>
+      </nav>
+    </div>
+  </div>`;
+  document.getElementById('navbar-container').innerHTML = navbarHTML;
 });
